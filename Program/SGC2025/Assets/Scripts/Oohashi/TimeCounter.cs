48329b764128c5cb196d619dcad31847e8f08152
SoltyPurin <Shouta1109s@gmail.com>
2025-10-18 19:27:48 +0900

    看板のimage、スコアのイメージ、サイロを登録 生成時間を可変にした

--- Program/SGC2025/Assets/Scripts/Oohashi/TimeCounter.cs
+++ Program/SGC2025/Assets/Scripts/Oohashi/TimeCounter.cs
@@ -4,13 +4,22 @@ public class TimeCounter : MonoBehaviour
 {
     [SerializeField, Header("障害物のマネージャー")]
     private ObstacleManager _obstacleManager = default;
+    [SerializeField,Header("スポーンのマネージャー")]
+    private SpawnManager _spawnManager = default;
+    [SerializeField, Header("何秒ごとにスピードアップさせるか")]
+    private float _speedUpEveryTime = 10;
+    [SerializeField, Header("何秒ごとにスポーンさせるか")]
+    private float _spawnTime = 5;
+    [SerializeField, Header("障害物生成時間をどれだけ短くするか")]
+    private float _shorteningTime = 0.01f;
     private float _initTime = 0;
     private bool _canChangeSpeed = true;
+    private bool _canSpawn = true;
     private bool _isFirst = true;
     public void Timer()
     {
         _initTime += Time.deltaTime;
-        bool canChangeSpeed = _initTime % 10 <= 0.1f && _canChangeSpeed;
+        bool canChangeSpeed = _initTime % _speedUpEveryTime <= 0.1f && _canChangeSpeed;
         if (canChangeSpeed)
         {
             if (_isFirst)
@@ -24,9 +33,25 @@ public class TimeCounter : MonoBehaviour
             _canChangeSpeed = false;
         }
 
-        if (_initTime % 10 > 0.1f)
+        if (_initTime % _speedUpEveryTime > 0.1f)
         {
             _canChangeSpeed = true;
+        }
+
+        bool canSpawn = _initTime % _spawnTime <= 0.1f && _canSpawn;
+        if (canSpawn)
+        {
+            Debug.Log("スポーン");
+            _spawnManager.RandomSpawn();
+            _canSpawn = false;
+            _spawnTime -= _shorteningTime;
+            _spawnTime *= 10;
+            _spawnTime = Mathf.Floor(_spawnTime) / 10;
+            _spawnTime = Mathf.Clamp(_spawnTime,0.5f,_spawnTime); 
+        }
+        if(_initTime % _spawnTime > 0.1f)
+        {
+            _canSpawn = true;
         }
     }
 }
