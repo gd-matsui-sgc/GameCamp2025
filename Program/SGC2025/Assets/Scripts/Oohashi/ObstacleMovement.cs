54679765f444b194ccbed8b03259ab2f45d8c9ce
SoltyPurin <Shouta1109s@gmail.com>
2025-10-18 15:22:46 +0900

    ボーン入りの鶏、敵及び味方の鶏のモデルを導入

--- Program/SGC2025/Assets/Scripts/Oohashi/ObstacleMovement.cs
+++ Program/SGC2025/Assets/Scripts/Oohashi/ObstacleMovement.cs
@@ -8,10 +8,16 @@ public class ObstacleMovement : MonoBehaviour
     #endregion
     #region ïœêî
     private Rigidbody _rigidBody = default;
+    private float _originDistance = default;
     #endregion
 
     public void GetComponentProtocol()
     {
         _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void MoveProtocol()
+    {
+        _rigidBody.linearVelocity = new Vector3(_moveDistance, 0, 0);
     }
 }
