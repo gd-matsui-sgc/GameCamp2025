94ef7e68f02b7a6f4b56aabd67b5c6407174dd32
SoltyPurin <Shouta1109s@gmail.com>
2025-10-19 10:58:25 +0900

    スコアの数字が変になるバグを修正

---
+++ Program/SGC2025/Assets/Scripts/Oohashi/FloorMove.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FloorMove : MonoBehaviour
+{
+    private Renderer _renderer = default;
+    [SerializeField, Header("‚Ç‚ê‚­‚ç‚¢‚Ì‘¬“x‚ÅˆÚ“®‚³‚¹‚é‚©")]
+    private float _floorMoveSpeed = 0.1f;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        float offset = _floorMoveSpeed * Time.time * -1;
+        _renderer.material.SetTextureOffset("_MainTex", new Vector2(0,offset));
+    }
+}
